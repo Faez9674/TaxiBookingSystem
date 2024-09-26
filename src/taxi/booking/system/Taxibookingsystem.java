ff3d260bd8dbffbdcdd85e9e2633a906d76a78ec
Faez9674 <129656122+Faez9674@users.noreply.github.com>
2024-09-27 01:11:40 +0530

    Add files via upload

---
+++ src/taxi/booking/system/Taxibookingsystem.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taxi.booking.system;
+
+/**
+ *
+ * @author Admin
+ */
+public class Taxibookingsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        DatabaseConnector dbConnector = new DatabaseConne
+//        SqlConnection con = new SqlConnection();
+//        SqlConnection isconnected = con.getConnection();
+//        
+    }
+    
+}
